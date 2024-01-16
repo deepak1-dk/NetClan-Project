5e724e2218f6db6fb566fb7c4331ec6e5cb1b59d
deepak1-dk <deepakkhatana7520@gmail.com>
2024-01-16 13:53:00 +0530

    Manage word count in Refine Activity

--- app/src/main/java/com/example/netclanproject/RefineActivity.kt
+++ app/src/main/java/com/example/netclanproject/RefineActivity.kt
@@ -3,29 +3,43 @@ package com.example.netclanproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
 import android.view.View
 import com.example.netclanproject.databinding.ActivityRefineBinding
 
 class RefineActivity : AppCompatActivity() {
-    private val binding : ActivityRefineBinding by lazy {
+    private val binding: ActivityRefineBinding by lazy {
         ActivityRefineBinding.inflate(layoutInflater)
     }
+
+    // private val WORD_LIMIT = 250
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.backButton.setOnClickListener {
-            /*startActivity(Intent(this,MainActivity::class.java))
-            finish()*/
             onBackPressed()
-
         }
+
+        binding.inputText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var data : String = binding.inputText.text.toString()
+                var length : Int? = (data as? String)?.length
+                binding.count.text = length.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+
+
     }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
-
-
 }
